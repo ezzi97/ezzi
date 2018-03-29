df916f0c3fc196991e48f90ddc8aadc57dac56c0
ezzi97 <37906497+ezzi97@users.noreply.github.com>
2018-03-29 22:44:29 +0200

    Create HandlerBot.py

---
+++ HandlerBot.py
@@ -0,0 +1,56 @@
+import json
+import urllib
+
+import requests
+from chatterbot import ChatBot
+
+class HandlerBot(ChatBot):
+    def __init__(self, TOKEN, URL):
+        self.TOKEN=TOKEN
+        self.URL=URL
+        super().__init__('ezzi', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
+
+    def get_url(self, url):
+        response = requests.get(url)
+        content = response.content.decode("utf8")
+        return content
+
+    def get_json_from_url(self, url):
+        content = self.get_url(url)
+        js = json.loads(content)
+        return js
+
+    def get_updates(self, offset=None):
+        url = self.URL + "getUpdates"
+        if offset:
+            url += "?offset={}".format(offset)
+        js = self.get_json_from_url(url)
+        return js
+
+    def get_last_update_id(self, updates):
+        update_ids = []
+        for update in updates["result"]:
+            update_ids.append(int(update["update_id"]))
+        return max(update_ids)
+
+    def echo_all(self, updates):
+        for update in updates["result"]:
+            try:
+                text = update["message"]["text"]
+                chat = update["message"]["chat"]["id"]
+                whatToSend = super().get_response(text)
+                self.send_message(whatToSend, chat)
+
+            except Exception as e:
+                print(e)
+
+    def get_last_chat_id_and_text(self, updates):
+        num_updates = len(updates["result"])
+        last_update = num_updates - 1
+        text = updates["result"][last_update]["message"]["text"]
+        chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+        return (text, chat_id)
+
+    def send_message(self, text, chat_id):
+        url = self.URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+        self.get_url(url)
