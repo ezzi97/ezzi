db90a1947f5b63b047107eebb1947b0ddd40a138
ezzi97 <37906497+ezzi97@users.noreply.github.com>
2018-03-29 22:57:18 +0200

    Update HandlerBot.py

--- HandlerBot.py
+++ HandlerBot.py
@@ -8,7 +8,7 @@ class HandlerBot(ChatBot):
     def __init__(self, TOKEN, URL):
         self.TOKEN=TOKEN
         self.URL=URL
-        super(self).__init__('Ron Obvious', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
+        super(HandlerBot, self).__init__('Ron Obvious', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
 
     def get_url(self, url):
         response = requests.get(url)
